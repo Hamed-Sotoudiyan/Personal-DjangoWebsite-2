582938f16ef2688b7af249eeebd4130daf08f316
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-01-14 19:27:50 +0330

    Update views.py

--- about/views.py
+++ about/views.py
@@ -4,6 +4,7 @@ from .models import about_detail
 # Create your views here.
 
 # rendering AboutUs Info (about_detail) to template
+# all of html files are in templates folder in a main root of project
 def main(request):
     context = {
         'about_detail_for_template' : about_detail.objects.last()
