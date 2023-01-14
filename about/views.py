9eff2b4968f96611a84ab0e51930d159661f2b0f
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-01-14 19:17:33 +0330

    Update views.py

--- about/views.py
+++ about/views.py
@@ -3,7 +3,7 @@ from .models import about_detail
 
 # Create your views here.
 
-
+# rendering AboutUs Info (about_detail) to template
 def main(request):
     context = {
         'about_detail_for_template' : about_detail.objects.last()
