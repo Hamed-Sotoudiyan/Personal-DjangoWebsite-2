de38ca03214270a0d7fbdf4c24dc2c7d2b11cfea
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-01-29 12:29:00 +0330

    Update urls.py

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'article'
 
+# one page for showing parent of article models atributes and another for details
+
 urlpatterns = [
     path('', views.main, name='main'),
     path('detail/<int:pk>/', views.detail, name='detail'),
