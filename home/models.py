a3226b3876a89fec5f6b158235760aa8d5733a95
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-02-04 11:17:31 +0330

    Update models.py

--- home/models.py
+++ home/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
+# for example - the elementary features of HomePage
+
 class Home (models.Model):
     title1 = models.TextField(blank=False)
     title2 = models.TextField(blank=False)
