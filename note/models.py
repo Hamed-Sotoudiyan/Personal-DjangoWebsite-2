955733b03adf53665641a24e41c66b764a99d2ec
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-02-06 12:40:45 +0330

    Update models.py

--- note/models.py
+++ note/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# for example - the imaginary features of Notes
+
 class Notes (models.Model):
     date = models.CharField(max_length=50, blank=False)
     title = models.TextField(blank=False)
