c4b6a3b6ba4abdeb5bdb7b7263b031abf5d212c3
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-02-06 12:40:14 +0330

    Update urls.py

--- note/urls.py
+++ note/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'note'
 
+# just two page for showing Notes models atributes
+
 urlpatterns = [
     path('', views.main, name='main'),
     path('detail/<int:pk>/', views.detail, name='detail'),
