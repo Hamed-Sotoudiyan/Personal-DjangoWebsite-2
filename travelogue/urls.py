f9b155d7c89c32d82729290a9eee4c7fcc34c046
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-02-11 10:58:47 +0330

    Update urls.py

--- travelogue/urls.py
+++ travelogue/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = 'travelogue'
 
+# just 2 pages for showing travelogue models atributes
+
+
 urlpatterns = [
     path('', views.main, name='main'),
     path('detail/<int:pk>/', views.detail, name='detail'),
